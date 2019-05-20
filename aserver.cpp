453017385e9c957ca81c493fffcd432d4b23a128
kwh44 <maistruk44@gmail.com>
2019-05-21 01:24:12 +0300

    minor fix

--- aserver.cpp
+++ aserver.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <mutex>
+#include <atomic>
 #include <streambuf>
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 #include <boost/asio/error.hpp>
 
-
 using namespace boost::asio;
-//static io_service service;
 
-class talk_to_client : public boost::enable_shared_from_this<talk_to_client>, boost::noncopyable {
+class server : public boost::enable_shared_from_this<server>, boost::noncopyable {
     ip::tcp::socket sock_;
     streambuf read_buffer_;
     streambuf write_buffer_;
     bool started_;
 
-    typedef talk_to_client self_type;
+    typedef server self_type;
 
-    explicit talk_to_client(io_service &service) : sock_(service), started_(false) {}
+    explicit server(io_service &service) : sock_(service), started_(false) {}
 
 public:
     typedef boost::system::error_code error_code;
-    typedef boost::shared_ptr<talk_to_client> ptr;
+    typedef boost::shared_ptr<server> ptr;
 
     void start() {
         started_ = true;
@@ -29,7 +28,7 @@ public:
     }
 
     static ptr new_(io_service &service) {
-        ptr new_ptr(new talk_to_client(service));
+        ptr new_ptr(new server(service));
         return new_ptr;
     }
 
@@ -37,25 +36,14 @@ public:
         if (!started_) return;
         started_ = false;
         sock_.close();
+
     }
 
     size_t read_complete(const boost::system::error_code &err, size_t bytes) {
         const char *begin = buffer_cast<const char *>(read_buffer_.data());
         if (bytes == 0) return 1;
-        bool found = std::find(begin, begin + bytes, '\n') < begin + bytes;
+        bool found = std::find(begin, begin + bytes, '3') < begin + bytes;
         return found ? 0 : 1;
-        /*if (bytes == 8)std::terminate();
-        while (bytes > 0) {
-            char c = *begin;
-            std::cout << "c is " << c << std::endl;
-            if (c == '3') {
-                return 0;
-            } else {
-                --bytes;
-            }
-            ++begin;
-        }
-        return 1;*/
     }
 
     void do_read() {
@@ -72,36 +60,49 @@ public:
     }
 
     void on_read(const error_code &err, size_t bytes) {
-        if (!err) {
-            std::ostringstream out;
-            out << &read_buffer_;
-            std::string msg(out.str());
-            do_write(msg);
-        }
-        stop();
+        std::ostringstream out;
+        out << &read_buffer_;
+        std::string msg(out.str());
+        do_write(msg);
     }
 
-    void on_write(const error_code &err, size_t bytes) { do_read(); }
+    void on_write(const error_code &err, size_t bytes) { stop(); }
 
     ip::tcp::socket &sock() { return sock_; }
 };
 
-void on_connect(ip::tcp::acceptor &acceptor, io_service &service, talk_to_client::ptr client,
+void on_connect(ip::tcp::acceptor &acceptor, io_service &service, server::ptr client,
                 const boost::system::error_code &err) {
     client->start();
-    talk_to_client::ptr new_client = talk_to_client::new_(service);
+    server::ptr new_client = server::new_(service);
     acceptor.async_accept(new_client->sock(),
                           boost::bind(on_connect, std::ref(acceptor), std::ref(service), new_client, _1));
 }
 
 void handle_connection(io_service &service, ip::tcp::acceptor &acceptor) {
-    talk_to_client::ptr client = talk_to_client::new_(service);
+    server::ptr client = server::new_(service);
     acceptor.async_accept(client->sock(), boost::bind(on_connect, std::ref(acceptor), std::ref(service), client, _1));
 }
 
-int main(int argc, char *argv[]) {
-    io_service service;
-    ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8001));
-    handle_connection(service, acceptor);
+void run_service(io_service &service) {
     service.run();
+}
+
+int main(int argc, char *argv[]) {
+
+    io_service service;
+
+    std::vector<ip::tcp::acceptor> acceptor_array;
+
+    ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8001));
+
+
+    for (int i = 0; i < 10; ++i) handle_connection(service, acceptor);
+
+    boost::thread_group thread_pool;
+    for (int i = 0; i < 10; ++i) {
+        thread_pool.create_thread([&service]() { run_service(service); });
+    }
+    thread_pool.join_all();
+    return 0;
 }
